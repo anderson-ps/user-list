7dd5e4804bebcdee82d24e921f92ce3642af5f88
Anderson Pires <andersonpires855@gmail.com>
2022-07-13 14:42:56 -0300

    Foi criado o componente user-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { UserFormComponent } from './pages/users/user-form/user-form.component';
 import { UsersListComponent } from './pages/users/users-list/users-list.component';
 
 @NgModule({
@@ -11,6 +12,7 @@ import { UsersListComponent } from './pages/users/users-list/users-list.componen
     AppComponent,
     HeaderComponent,
     UsersListComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
