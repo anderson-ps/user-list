c45b7e516355dea6ad1e5ed3253d167d4aa7bbd7
Anderson Pires <andersonpires855@gmail.com>
2022-07-14 13:56:01 -0300

    foi desenvolvido o metodo para salvar o usuario no banco dde dados

--- src/app/pages/users/user-form/user-form.component.ts
+++ src/app/pages/users/user-form/user-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-form',
@@ -9,8 +11,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class UserFormComponent implements OnInit {
   
   userForm: FormGroup;
+  users: Array<User> = [];
 
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder, private userService:UserService) { 
     this.userForm = this.fb.group({
       id: 0,
       nome: '',
@@ -23,8 +26,17 @@ export class UserFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getUsers(){
+    this.userService.getUsers().subscribe(response => {
+      this.users = response;
+    })
+  }
+
   creatUser(){
-    
+    this.userForm.get('id')?.patchValue(this.users.length + 1);
+    this.userService.postUser(this.userForm.value).subscribe(result => {
+      console.log(`Usuario ${result.nome} ${result.sobrenome} Cadastrado com sucesso!`)
+    })
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
@@ -9,11 +9,20 @@ import { User } from '../models/user';
 export class UserService {
 
   apiUrl = 'https://sheet.best/api/sheets/57b50cae-c579-407f-80fe-81b86f3b3017';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'content-Type': 'application/json',
+    })
+  }
 
   constructor(private httpClient:HttpClient) { }
 
   getUsers():Observable<User[]>{
     return  this.httpClient.get<User[]>(this.apiUrl);
 
+  }
+
+  postUser(user:User):Observable<User>{
+    return this.httpClient.post<User>(this.apiUrl, user,this.httpOptions);
   }
 }
